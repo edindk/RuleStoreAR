09922bf7483f54b81bc2120f717a5c23fa611b26
edindk <edindkk@gmail.com>
2021-04-19 11:38:12 +0200

    Vis produkter

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        products: 'hello'
+    },
+    mutations: {},
+    actions: {
+        getProducts(context, data) {
+            return new Promise((resolve, reject) => {
+                axios
+                    .get('/products')
+                    .then(response => {
+                        resolve(response)
+                    })
+                    .catch(error => {
+                        reject(error)
+                    })
+            })
+        }
+    },
+    modules: {}
 })
